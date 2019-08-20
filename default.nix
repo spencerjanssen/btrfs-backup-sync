b25b0d60a4774dc1543d82676307857819dfecb1
Spencer Janssen <spencerjanssen@gmail.com>
2019-08-20 09:46:56 -0500

    Take nixpkgs as an argument

--- default.nix
+++ default.nix
@@ -1,6 +1,5 @@
-{}:
-let nixpkgs = import <nixpkgs> { };
-    btrfs-backup-sync = nixpkgs.haskellPackages.callCabal2nix "btrfs-backup-sync" ./. {};
+{nixpkgs ? import <nixpkgs> { }}:
+let btrfs-backup-sync = nixpkgs.haskellPackages.callCabal2nix "btrfs-backup-sync" ./. {};
 in
 {
     inherit btrfs-backup-sync;
