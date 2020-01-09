eef6c93ed3d5da85d5435830b1af1ade6bf6fa4b
Spencer Janssen <spencerjanssen@gmail.com>
2020-01-09 09:57:20 -0600

    Update dependencies
    * 'bios' version of HIE is now the default
    * update niv

--- nix/package-set.nix
+++ nix/package-set.nix
@@ -11,7 +11,7 @@ let pkgs = import sources.nixpkgs (import sources."haskell.nix");
       ghc = pkgs.buildPackages.pkgs.haskell-nix.compiler.${haskellCompiler};
     };
     nivPkgs = (import sources.niv {});
-    hies = (import sources.all-hies {}).bios.selection {
+    hies = (import sources.all-hies {}).selection {
       selector = p: {
         ${haskellCompiler} = p.${haskellCompiler};
       };
